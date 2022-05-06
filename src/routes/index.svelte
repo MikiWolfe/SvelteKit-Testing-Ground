6d12575e31798b3a101bbd6b1b817649258332ef
Mikayla Bruce <mkla.w.b@gmail.com>
2022-05-06 13:29:10 -0700

    file management

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script>
+	let title = 'Testing ground';
+</script>
+
+<div>
+	<h1>I have no idea what I am doing</h1>
+	<p>Hello. I am having fun</p>
+</div>
+
+<style>
+	* {
+		background-color: linen;
+	}
+	h1 {
+		text-align: center;
+	}
+</style>
