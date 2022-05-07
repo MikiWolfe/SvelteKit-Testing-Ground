3e0b5acdb19d7a89c1546ac7fa18b30d60ed9308
Mikayla Bruce <mkla.w.b@gmail.com>
2022-05-06 19:38:05 -0700

    figured out how to make a box

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,34 @@
 <script>
-	let title = 'Testing ground';
+	
+	const random = (number) => {
+		return Math.floor(Math.random()*number);;
+}
+function randomColor(){
+ return 'rgb('+random(255)+','+random(255)+','+random(255)+')';  
+	}
 </script>
 
 <div>
 	<h1>I have no idea what I am doing</h1>
 	<p>Hello. I am having fun</p>
+<div class = 'box'></div>
+	<button on:click={randomColor}>Change color</button>
 </div>
-
+<p class="foot">Made by MikiWolfe 2022</p>
 <style>
 	* {
+		margin: 0;
 		background-color: linen;
 	}
 	h1 {
 		text-align: center;
+	}
+	.box {
+		height: 150px;
+		width: 150px;
+		background-color: blue;
+	}
+	.foot {
+text-align: center;
 	}
 </style>
