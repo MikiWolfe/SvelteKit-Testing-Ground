f4c6f04212d21a1dc5ed085b8d674e4879eddb1d
Mikayla Bruce <mkla.w.b@gmail.com>
2022-05-08 12:32:37 -0700

    deploying to pages

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,36 +1,55 @@
 <script>
-	
 	const random = (number) => {
-		return Math.floor(Math.random()*number);;
-}
-function randomColor(){
- let color = 'rgb('+random(255)+','+random(255)+','+random(255)+')'; 
- document.getElementById("box").style.backgroundColor = color 
+		return Math.floor(Math.random() * number);
+	};
+	function randomColor() {
+		let color = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+		document.getElementById('box').style.backgroundColor = color;
 	}
 </script>
 
 <div>
+	<title>Magic color changing box</title>
 	<h1>I have no idea what I am doing</h1>
-	<p>Hello. I am having fun</p>
-<div id = 'box' style ='height: 200px;
-width: 200px;
-border: 5px solid red; background-color: blue'></div>
-<button on:click={randomColor}>Change color</button>
-<p class="foot">Made by MikiWolfe 2022</p>
+	<p class="welcome">Hello! Click the button to change the color of the box.</p>
 
+	<div id="box" />
+
+	<button on:click={randomColor}>Click me!</button>
+	<p class="foot">Made by MikiWolfe 2022</p>
 </div>
-
 
 <style>
 	* {
-		margin: 0;
-		background-color: linen;
+		font-family: Helvetica, sans-serif;
+		margin: 0px;
+		background-color: rgb(240, 209, 240);
+		padding: 0.5%;
 	}
 	h1 {
 		text-align: center;
 	}
-
+	.welcome {
+		font-size: 20px;
+	}
+	#box {
+		height: 200px;
+		width: 200px;
+		border: 5px solid black;
+		background-color: rebeccapurple;
+		margin-bottom: 5px;
+	}
+	button {
+		font-size: 16px;
+		background-color: black;
+		color: whitesmoke;
+		cursor: pointer;
+	}
+	button:hover {
+		background-color: rgb(238, 238, 140);
+		color: black;
+	}
 	.foot {
-text-align: center;
+		text-align: center;
 	}
 </style>
